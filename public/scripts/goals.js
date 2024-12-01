1401d38d241a43b61140bb11a388988a6db2d26f
S4DSmile <164679492+S4DSmile@users.noreply.github.com>
2024-12-01 14:24:32 +0000

    tidying upand added goals.css

--- public/scripts/goals.js
+++ public/scripts/goals.js
@@ -8,20 +8,35 @@ if (sectionElement != null)
                 .then(responseData =>{
                     console.log(responseData);
                     for (item of responseData) {
+
+                        // creating html elements
                         const myGoals = document.createElement('article');
                         const imageElement = document.createElement('img');
                         const myTitle = document.createElement('h4');
                         const myGoal = document.createElement('p');
+                        const myTarget = document.createElement('ul');
+
+                        // appending article to html page element
                         sectionElement.appendChild(myGoals)
+
+                        // set the class of the element to control presentation
                         myGoals.setAttribute('class', 'goals')
+
+                        // controlling the content of the elements
                         imageElement.src = item.imageURL;
                         imageElement.alt = item.alt;
                         myGoal.textContent = `${item.goal}`;
                         myTitle.textContent = `${item.title}`;
+                        myTarget.textContent = `${item.target}`;
+
+                        // appending elements to the article element created on the goals page
                         myGoals.appendChild(imageElement);
                         myGoals.appendChild(myTitle);
                         myGoals.appendChild(myGoal);
+                        myGoals.appendChild(myTarget);
                     }
+                    
+                    //error handling
                 }).catch(error => console.error("Error fetching JSON data:", error));;
             })
     }
